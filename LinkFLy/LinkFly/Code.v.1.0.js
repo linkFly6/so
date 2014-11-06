bcf10c138227d40b4794fb410cf8e01a84718062
linkFly <linkFly6@live.com>
2014-11-06 18:07:52 +0800

    update

--- LinkFLy/LinkFly/Code.v.1.0.js
+++ LinkFLy/LinkFly/Code.v.1.0.js
@@ -167,19 +167,33 @@
     //——————————————————————————————————Array数组结束——————————————————————————————————————
 
 
+    //格式化字符串方法，使用：${0},${1}、${name},${value}
     var format = function (str, object) {
+        /// <summary>
+        ///     1: format(str,object) - 格式化一组字符串，参阅C# string.format()
+        ///     &#10;    1.1 - format(str,object) - 通过对象格式化
+        ///     &#10;    1.2 - format(str,Array) - 通过数组格式化
+        /// </summary>
+        /// <param name="str" type="String">
+        ///     格式化模板(字符串模板)
+        /// </param>
+        /// <param name="object" type="Object">
+        ///     Object:使用对象的key格式化字符串，模板中使用${name}占位：${data},${value}
+        ///     Array:使用数组格式化，模板中使用${Index}占位：${0},${1}
+        /// </param>
+        /// <returns type="String" />
         var array = Array.prototype.slice.call(arguments, 1);
         //可以被\符转义
-        return str.replace(/\\?\${([^{}])\}/gm, function (match, key) {
+        return str.replace(/\\?\${([^{}]+)\}/gm, function (match, key) {
             //匹配转义符"\"
             if (match.charAt(0) == '\\')
                 return match.slice(1);
             var index = Number(key);
             if (index >= 0)
                 return array[index];
-            return object[key]!==undefined ? object[key] : match;
+            return object[key] !== undefined ? object[key] : match;
         });
-    }
+    };
 
 
 
