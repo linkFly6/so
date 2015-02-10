4d21c4473b12195347fc9c814656c65e5d696f86
[ Dying In The Sun ] <linkFly6@live.com>
2015-02-10 22:55:31 +0800

    结构调整，编写Defer，调整Defer定位

--- LinkFLy/Code/Defer/Defer.js
+++ LinkFLy/Code/Defer/Defer.js
@@ -9,27 +9,33 @@
             return toString.call(obj) === "[object Function]";
         },
         isOver = function (state) {//defer对象是否已经完成状态
-            return state === 'rejected' && state === 'resolved';
+            return state !== globalState.PENDING;
         },
         fire = function (callbacks, datas) {//执行一组函数，并允许追加数据
             if (!callbacks || !callbacks.length) return;
             for (var i = 0, len = callbacks.length; i < len; i++) {
                 datas ? callbacks[i].apply(datas) : callbacks[i];//设计不正确，每个函数的返回值都应该追加到数组中
             }
+        },
+        checkState = function () {
+
+        },
+        globalState = {
+            //3个状态是不够的，一共有5个状态：成功，失败，成功等待中，失败等待中
+            PENDING: 0,
+            RESOLVED: 1,
+            REJECTED: 2,
+            RESOLVEWAIT: 3,
+            REJECTWAIT: 4
         };
+    //只有一个要求！！！轻！！！
     var Defer = function (callback) {
-        //3个状态是不够的，一共有5个状态：成功，失败，成功等待中，失败等待中
+        //要兼容jQuery的ajax语法....
+
         //Publish/Subscribe模式实现
-        var callbacks = [[], [], []],//done/fail/aways
-            state = 'pending',
-            pubDefers = [];//发布模式
+        var callbacks = [[], [], []],//done/fail/progress
+            state = globalState.PENDING;
         var defer = {
-            _childReady: function () {
-
-            },
-            _parentReady: function () {
-
-            },
             state: function () {
                 return state;
             },
@@ -37,45 +43,43 @@
                 return Defer(function (newDefer) {
                     //子链只会有一个父链，只会订阅一个父链
                     newDefer.parent = defer;
-                    //子链和父链别应该通过事件订阅模式完成，子链可以访问父链
-                    pubDefers.push(newDefer);
                 });
             },
-            aways: function () {
-
+            done: function () {
+                if (state === globalState.RESOLVED) {
+                    fire(arguments);
+                } else
+                    each.call(arguments, function (fn) {
+                        if (isFunction(fn))
+                            callbacks[0].push(fn);
+                    });
+                return this;
+            },
+            fial: function () {
+                return this;
+            },
+            aways: function (resolve, reject) {
+                return defer.done(resolve).fail(reject);
             },
             resolve: function () {
                 //执行时机要判断父链和子链是否完成
-                state = 'resolved';
                 //如果有父链，则订阅父链的信息
                 if (defer.parent == null || isOver(defer.parent.state())) {
+                    state = globalState.RESOLVED;
                     fire(callbacks[0], arguments);
                 } else {
                     //给父链发消息通知它已经完成了
-
+                    state = globalState.RESOLVEWAIT;
+                    defer.parent.aways(function () {
+                        defer.resolve();
+                    });
                 }
             },
             reject: function () {
 
             }
-
         };
-    };
-
-    Defer.prototype.then = function (done, fail) {
-        return Defer(function () {
-
-        }, function () {
-
-        });
-    };
-    Defer.prototype.resolve = function () {
-
-        return this;
-    };
-    Defer.prototype.reject = function () {
-
-        return this;
+        return defer;
     };
     window.Defer = Defer;
 })(window);
